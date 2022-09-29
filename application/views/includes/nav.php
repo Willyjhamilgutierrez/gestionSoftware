2d15636193d78f6a7f895e71c64bb97bccc69fed
willyjhamilgutierrez <gutierrez.willy.601@gmail.com>
2022-09-29 15:14:54 -0400

    cambio boton negro

--- application/views/includes/nav.php
+++ application/views/includes/nav.php
@@ -196,7 +196,7 @@
                                     class="hide-menu">CREAR CRITERIO</span></a></li>
                         <li class="text-center p-40 upgrade-btn">
 						<li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                href="<?php echo base_url(); ?>" aria-expanded="false"><i class="mdi mdi-alert-outline"></i><span
+                                href="<?php echo base_url(); ?>" aria-expanded="false"><i class="mdi mdi-alert"></i><span
                                     class="hide-menu">CALIFICAR</span></a></li>
                         <li class="text-center p-40 upgrade-btn">
 						<li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
